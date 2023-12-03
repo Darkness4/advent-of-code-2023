f28db4468df862c455e268df147c2dc8b7ee66e1
Nguyen Marc <nguyen_marc@live.fr>
2023-12-03 20:12:56 +0100

    chore(day2): add various optimizations

--- src/day2.zig
+++ src/day2.zig
@@ -12,50 +12,35 @@ fn day2() !i64 {
             continue;
         }
         // Parse Game ID
-        var id: i64 = 0;
-        var it = std.mem.splitSequence(u8, line, ": ");
-        if (it.next()) |x| {
-            var gameit = std.mem.splitSequence(u8, x, " ");
-            _ = gameit.next(); // "Game: "
-            if (gameit.next()) |idstr| {
-                id = try std.fmt.parseInt(i64, idstr, 10);
-            }
-        }
+        const gamedel = std.mem.indexOf(u8, line, ":").?;
+        const id = try std.fmt.parseInt(i64, line[5..gamedel], 10);
+
         // Parse the Game
         const max_red = 12;
         const max_green = 13;
         const max_blue = 14;
-        if (it.next()) |game| {
-            var roundit = std.mem.splitSequence(u8, game, "; ");
-            while (roundit.next()) |round| {
-                var handit = std.mem.splitSequence(u8, round, ", ");
-                var red: i64 = 0;
-                var green: i64 = 0;
-                var blue: i64 = 0;
+        const game = line[gamedel + 2 .. line.len];
+        var roundit = std.mem.splitSequence(u8, game, "; ");
+        while (roundit.next()) |round| {
+            var handit = std.mem.splitSequence(u8, round, ", ");
+            var red: i64 = 0;
+            var green: i64 = 0;
+            var blue: i64 = 0;
 
-                // Parse "x green, y blue, z red"
-                while (handit.next()) |hand| {
-                    var n: i64 = 0;
-                    var v = std.mem.splitSequence(u8, hand, " ");
-                    if (v.next()) |nstr| {
-                        n = try std.fmt.parseInt(i64, nstr, 10);
-                    }
-                    if (v.next()) |typ| {
-                        if (std.mem.eql(u8, typ, "red")) {
-                            red = n;
-                        }
-                        if (std.mem.eql(u8, typ, "green")) {
-                            green = n;
-                        }
-                        if (std.mem.eql(u8, typ, "blue")) {
-                            blue = n;
-                        }
-                    }
+            // Parse "x green, y blue, z red"
+            while (handit.next()) |hand| {
+                const handdel = std.mem.indexOf(u8, hand, " ").?;
+                const n = try std.fmt.parseInt(i64, hand[0..handdel], 10);
+                switch (hand[handdel + 1]) {
+                    'r' => red = n,
+                    'g' => green = n,
+                    'b' => blue = n,
+                    else => unreachable,
                 }
+            }
 
-                if (red > max_red or green > max_green or blue > max_blue) {
-                    continue :outer;
-                }
+            if (red > max_red or green > max_green or blue > max_blue) {
+                continue :outer;
             }
         }
         // Finished parsing, game is valid
@@ -74,49 +59,32 @@ fn day2p2() !i64 {
         if (line.len == 0) {
             continue;
         }
-        // Parse Game ID
-        var id: i64 = 0;
-        var it = std.mem.splitSequence(u8, line, ": ");
-        if (it.next()) |x| {
-            var gameit = std.mem.splitSequence(u8, x, " ");
-            _ = gameit.next(); // "Game: "
-            if (gameit.next()) |idstr| {
-                id = try std.fmt.parseInt(i64, idstr, 10);
-            }
-        }
+        const gamedel = std.mem.indexOf(u8, line, ":").?;
+
         // Parse the Game
         var req_red: i64 = 0;
         var req_green: i64 = 0;
         var req_blue: i64 = 0;
-        if (it.next()) |game| {
-            var roundit = std.mem.splitSequence(u8, game, "; ");
-            while (roundit.next()) |round| {
-                var handit = std.mem.splitSequence(u8, round, ", ");
+        const game = line[gamedel + 2 .. line.len];
+        var roundit = std.mem.splitSequence(u8, game, "; ");
+        while (roundit.next()) |round| {
+            var handit = std.mem.splitSequence(u8, round, ", ");
 
-                // Parse "x green, y blue, z red"
-                while (handit.next()) |hand| {
-                    var n: i64 = 0;
-                    var v = std.mem.splitSequence(u8, hand, " ");
-                    if (v.next()) |nstr| {
-                        n = try std.fmt.parseInt(i64, nstr, 10);
-                    }
-                    if (v.next()) |typ| {
-                        if (std.mem.eql(u8, typ, "red")) {
-                            if (req_red <= n) {
-                                req_red = n;
-                            }
-                        }
-                        if (std.mem.eql(u8, typ, "green")) {
-                            if (req_green <= n) {
-                                req_green = n;
-                            }
-                        }
-                        if (std.mem.eql(u8, typ, "blue")) {
-                            if (req_blue <= n) {
-                                req_blue = n;
-                            }
-                        }
-                    }
+            // Parse "x green, y blue, z red"
+            while (handit.next()) |hand| {
+                const handdel = std.mem.indexOf(u8, hand, " ").?;
+                const n = try std.fmt.parseInt(i64, hand[0..handdel], 10);
+                switch (hand[handdel + 1]) {
+                    'r' => if (req_red < n) {
+                        req_red = n;
+                    },
+                    'g' => if (req_green < n) {
+                        req_green = n;
+                    },
+                    'b' => if (req_blue < n) {
+                        req_blue = n;
+                    },
+                    else => unreachable,
                 }
             }
         }
